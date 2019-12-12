1fd029c16c305b6ae3a1cbea5d527f861340aaf6
xian96 <42896816+xian96@users.noreply.github.com>
2019-12-12 14:57:11 -0500

    create the signup router

--- data/user.js
+++ data/user.js
@@ -41,6 +41,65 @@ module.exports = {
         } catch (e) {
             throw e;
         }
-    }
+    },
+    
+    async getUserByUsernameOrEmail(username, email) {
+        const userCollection = await user();
+        const currUser = await userCollection.findOne({ $or: [{ username: username }, { email: email }] });
+        return currUser;
+    },
+
+    async createHashedPassword(password) {
+        try {
+            if (!password)
+                throw "Password not given";
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            return hashedPassword;
+        } catch (e) {
+            throw e;
+        }
+    },
+
+    async getUserById(id) {
+        if (!id)
+            throw "You must provide a id for your post";
+        else if (!ObjectID.isValid(id)) {
+            if (typeof id === 'string') {//id type is 'string' you'll have to convert into ObjectID 
+                id = ObjectID(id);
+            }
+            else {
+                throw `id:${id}, Must Be STRING OR OBJECT ID`;
+            }
+        }
+
+        const userCollection = await user();
+        const userGet = await userCollection.findOne({ _id: ObjectID(id) });
+        if (userGet === null)
+            throw "No user with that id";
+
+        return userGet;
+    },
+
+    async addUser(firstName, lastName, email, username, hashedPassword) {
+        //need error checking here
+        let newUser = {
+            // _id: ,
+            username: username.toLowerCase(),
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            gender: gender,
+            city: city,
+            state: state,
+            age: age,
+            password: hashedPassword,
+            pollsCreated: [],
+            pollsVotedIn: []
+        };
+        const userCollection = await user();
+        const newInsertInformation = await userCollection.insertOne(newUser);
+        const newId = await newInsertInformation.insertedId;
+        await this.getUserById(newId);
+    },
 };
 
